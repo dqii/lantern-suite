cb32a1e7610ca059808e925db77f30d90bd919cb
Varik Matevosyan <varikmatevosyan@gmail.com>
2024-05-01 13:00:53 -0700

    Update pgbouncer config

--- scripts/run-pgbouncer.sh
+++ scripts/run-pgbouncer.sh
@@ -1,7 +1,8 @@
 set -euo pipefail
 
-cd /tmp
+PG_SOCKET_DIR=/var/run/postgresql
+cd $PG_SOCKET_DIR
 /opt/configure-pgbouncer.sh
-rm -rf /tmp/pgbouncer.pid || true
-pgbouncer /tmp/pgbouncer.ini 2>&1 | sed -u -e 's/^/[PGBOUCNER]: /'
+rm -rf $PG_SOCKET_DIR/pgbouncer.pid || true
+pgbouncer $PG_SOCKET_DIR/pgbouncer.ini 2>&1 | sed -u -e 's/^/[PGBOUCNER]: /'
 echo "Running pgbouncer in the background"
