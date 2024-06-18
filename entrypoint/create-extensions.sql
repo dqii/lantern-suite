2abfbd39d187027c29196f22e38bf255dd0d6dbb
Di Qi <di@lantern.dev>
2024-06-17 17:09:12 -0700

    Add pgvector (#15)

--- entrypoint/create-extensions.sql
+++ entrypoint/create-extensions.sql
@@ -1,4 +1,5 @@
 create extension pg_stat_statements;
 create extension pg_cron;
+create extension vector;
 create extension lantern;
 create extension lantern_extras;
--- test/extension-count.sh
+++ test/extension-count.sh
@@ -1,8 +1,8 @@
-count=$(PGPASSWORD=postgres psql -h lantern -U postgres -t -A -c "SELECT COUNT(*) FROM pg_available_extensions WHERE name IN ('lantern', 'lantern_extras', 'pg_cron', 'pg_stat_statements');")
+count=$(PGPASSWORD=postgres psql -h lantern -U postgres -t -A -c "SELECT COUNT(*) FROM pg_available_extensions WHERE name IN ('lantern', 'lantern_extras', 'pg_cron', 'pg_stat_statements', 'vector');")
 echo "Count: $count"
 if echo "$count" | grep -q "ERROR"; then
   echo "Failed to retrieve extension count" && exit 1
-elif [ "$count" -ne 4 ]; then
+elif [ "$count" -ne 5 ]; then
   echo "Extension check failed" && exit 1
 else
   echo "Extension check passed"
