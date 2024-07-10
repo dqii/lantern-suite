03a9132522f2d4c2efd3a260ce090d40dc9ad0db
Di Qi <di@lantern.dev>
2024-07-10 16:23:36 -0700

    Add .so test

---
+++ test/so-files-present.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PG_VERSION=15
+
+declare -a extensions=("lantern" "lantern_extras" "pg_cron" "pg_stat_statements" "vector")
+
+missing_files=()
+for ext in "${extensions[@]}"; do
+  if ! [ -f "/usr/lib/postgresql/$PG_VERSION/lib/${ext}.so" ]; then
+    missing_files+=("/usr/lib/postgresql/$PG_VERSION/lib/${ext}.so")
+  fi
+done
+
+if [ ${#missing_files[@]} -ne 0 ]; then
+  echo "Shared object files missing for the following extensions:"
+  for file in "${missing_files[@]}"; do
+    echo "$file"
+  done
+  exit 1
+else
+  echo "All necessary shared object files are present"
+fi
